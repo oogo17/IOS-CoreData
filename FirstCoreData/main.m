6218a64db882dfaaa197deb81e7adc91ba5ac8fe
omar <oogomx_1707@hotmail.com>
2013-08-15 01:08:56 -0500

    Initial Commit

---
+++ FirstCoreData/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FirstCoreData
+//
+//  Created by Omar Gonzalez ochoa on 15/08/13.
+//  Copyright (c) 2013 Omar Gonzalez ochoa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
